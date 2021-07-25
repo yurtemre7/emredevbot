73a449aeec1e93f7b2d60c1d94a48c1e9cb74603
yurtemre7 <emre.yurtseven.1@campus.tu-berlin.de>
2021-07-25 19:21:15 +0200

    added help function + better minimize description

--- emredev_bot.py
+++ emredev_bot.py
@@ -1,7 +1,7 @@
 from telegram.ext import Updater, MessageHandler, Filters
 from telegram import Bot
 from threading import Thread
-from keys import TELEGRAM_BOT_API_KEY as api
+from keys import TELEGRAM_BOT_API_KEY as api, emre_telegram_id, teoman_telegram_id
 import forsa as fs
 import uni_klausur_functions as kf
 
@@ -15,6 +15,8 @@ def echo(u, c):
     msg = u.message.text.lower()
     cid = u.message.chat.id
 
+    print(cid)
+
     if 'emre' in msg:
         emredev.send_message(cid, "@emredev ist der Entwickler von mir!")
 
@@ -24,15 +26,21 @@ def echo(u, c):
     if 'isda' in msg:
         emredev.send_message(
             cid, "Ich halte ausschau nach der ISDA Note von Emre!")
+
+    if '/help' in msg:
+        emredev.send_message(
+            cid, "Hier sind alle meine Befehle: /help, /isda, /emre, /teo, /minimize und /cyk")
     # minimize 5
     if '/minimize' in msg:
         # parse string "minimize 5" to int 5
         i = msg.split(' ')
         if len(i) < 5:
             emredev.send_message(
-                cid, "Schreibe bitte den Index des größten Zustandes. z.B.: /minimize 8 a,b q0 q1")
+                cid, "z.B.: /minimize 7 a,b q0 q3\n\nErklärung:\n7 = größter Index der Zustände zb q7 => 7\na,b = Eingabealphabet\nq0 = Startzustand / q0,q1 Startzustände\nq3 = Endzustand / q3,q5 Endzustände\n")
         else:
             fs.tschia_minimize(emredev, i, cid)
+            emredev.send_message(
+                cid, "Vergiss nicht das Delta anzupassen! zb. qA0 -> q0. Die 'A's sind zum Ersetzen da.")
     if '/cyk' in msg:
         # parse string "minimize 5" to int 5
         i = msg.split(' ')
@@ -51,7 +59,8 @@ def main():
     dp = updater.dispatcher
     dp.add_handler(MessageHandler(Filters.all, echo))
     updater.start_polling()
-    print('emredev.py startet!')
+    emredev.send_message(emre_telegram_id, 'emredev.py startet!')
+    emredev.send_message(teoman_telegram_id, 'emredev.py startet!')
     # Thread(target=kf.look, args=(emredev,)).start()
 
 
