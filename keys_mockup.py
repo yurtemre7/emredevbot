d3b980d322bfd99f345fe922680c21ed0a859261
yurtemre7 <emre.yurtseven.1@campus.tu-berlin.de>
2021-08-03 16:51:28 +0200

    keys mockup

---
+++ keys_mockup.py
@@ -0,0 +1,8 @@
+# Benenne die Datei in "keys.py" um und setzte deine entsprechenden Daten ein.
+
+TELEGRAM_BOT_API_KEY = ''  # TELEGRAM BOT API KEY
+
+cookie = 'Cookies vom Browser bei isis'
+
+# optional deine telegram id hardcoden
+deine_telegram_id = 00000000
