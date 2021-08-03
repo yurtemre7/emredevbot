45ae67c93d899af66477be79fa63941e8eba622e
yurtemre7 <emre.yurtseven.1@campus.tu-berlin.de>
2021-08-03 10:16:56 +0200

    helping, ids, notify and logging only commands

--- emredev_bot.py
+++ emredev_bot.py
@@ -12,9 +12,44 @@ import uni_klausur_functions as kf
 emredev = Bot(api)
 
 
-def cmd_handling(msg, cid, msg_orig):
+def helping(cid, msg):
+    i = msg.split(' ')
+    if len(i) > 1:
+        cmd_helps = {
+            'minimize': 'Minimiert einen DFA\nz.B.: /minimize 7 a,b q0 q3\n\nErklärung:\n7 = größter Index der Zustände zb q7 => 7\na,b = Eingabealphabet\nq0 = Startzustand / q0,q1 Startzustände\nq3 = Endzustand / q3,q5 Endzustände\n',
+            'gruppe': 'Information zu Gruppen.',
+            'notify': 'Erhalte globale Nachrichten',
+            'denotify': 'Erhalte keine weiteren globalen Nachrichten',
+            'emre': 'Information zum Erfinder.',
+            'cyk': """CYK-Algorithmus von CNF-Grammatiken\nz.B.: /cyk baaba
+                        'S -> AB | BC\nA -> BA | a | b\nB -> CC | b\nC -> AB | a'""",
+            'crs': 'Chinesischer Restsatz\nz.B.: /crs 2,3 3,5 2,7',
+            'prf': 'Primfaktorzerlegung einer Zahl (n) und seine Eulersche Phi-Funtion\nz.B.: /prf 10',
+            'rsa_pkey': 'Berechnet den privaten Schlüssel bei gegebenen p und q und e.\nz.B.: /rsa_pkey 11 37 37',
+            'rsa_dec': 'Berechnet die originale Nachricht anhand eines öffentlichen Schlüssels (e, n) und der verschlüsselten Nachricht (c).\nz.B.: /rsa_dec 299 5 60',
+            'euk': 'Führt den erweiterten euklidischen Algorithmus aus bei gegebenen a und b.\nz.B.: /euk 2 9'
+
+        }
+        command = i[1]
+
+        if command not in cmd_helps:
+            emredev.send_message(cid, 'Keine Erklärung.. 🙁')
+            return
+
+        if command == 'crs':
+            emredev.send_photo(
+                cid, photo='https://cdn.discordapp.com/attachments/319066984748941312/871069126402142298/besteErklarung.jpg')
+        emredev.send_message(
+            cid, cmd_helps[command])
+    else:
+        emredev.send_message(
+            cid, 'Hier sind alle meine Befehle: /help, /daten, /notify, /denotify, /gruppe, /emre, /minimize, /cyk, /crs, /rsa_dec, /rsa_pkey, /euk und /prf.\nUm mehr über einen Befehl zu erfahren schreibe: z.B: /help prf\n\nZudem reagiere ich auf die Keywords "Java" und "Ente" sobald diese in einem Satz vorkommen. Probier es doch aus!')
+
+
+def cmd_handling(msg, cid, msg_orig, is_group):
     if msg == '/start':
         intro = 'Hi! Ich bin ein unoffizieller Telegram-TUB-Bot, made by @emredev, der Dir vieles einfacher macht.\n'
+        nachricht = 'Es wird in der Zukunft "Nachrichten an alle" geben. Du erhälst natürlich nur eine Nachricht, wenn Du dies willst. Schreibe /notify wenn Du dabei bist und ggf. /denotify wenn Du keine weiteren Nachrichten erhalten möchtest.'
         help = 'Schreibe /help um alle Befehle sehen zu können.\n'
         daten = 'Schreibe /daten, falls du weitere Infos zum Bot wissen möchtest.\n'
         emredev.send_message(cid, intro + help + daten)
@@ -40,6 +75,30 @@ def cmd_handling(msg, cid, msg_orig):
 
     elif '/log' in msg and cid == emre_telegram_id:
         emredev.send_document(cid, open('cache/log.txt', 'rb'))
+    
+    elif '/ids' in msg and cid == emre_telegram_id:
+        emredev.send_document(cid, open('cache/unique_ids.txt', 'rb'))
+
+    elif msg == '/notify' and not is_group:
+        with open('cache/unique_ids.txt', 'r+') as f:
+            # read file to list
+            unique_ids = list(f.read().splitlines())
+            # check if user is already in list
+            if str(cid) not in unique_ids and not is_group:
+                f.write(f'{cid}\n')
+        emredev.send_message(
+            cid, "Du erhältst nun globale Nachrichten. Schreibe /denotify, falls dies ausversehen war.")
+
+    elif msg == '/denotify' and not is_group:
+        lines = None
+        with open("cache/unique_ids.txt", "r") as f:
+            lines = f.readlines()
+        with open("cache/unique_ids.txt", "w") as f:
+            for line in lines:
+                if line.strip("\n") != f"{cid}":
+                    f.write(line)
+        emredev.send_message(
+            cid, "Du wirst ab sofort keine globalen Nachrichten erhalten.")
 
     elif '/sendall' in msg and cid == emre_telegram_id:
         i = msg_orig.split(' ')
@@ -54,35 +113,7 @@ def cmd_handling(msg, cid, msg_orig):
                 emredev.send_message(id, txt)
 
     elif '/help' in msg:
-        i = msg.split(' ')
-        if len(i) > 1:
-            cmd_helps = {
-                'minimize': 'Minimiert einen DFA\nz.B.: /minimize 7 a,b q0 q3\n\nErklärung:\n7 = größter Index der Zustände zb q7 => 7\na,b = Eingabealphabet\nq0 = Startzustand / q0,q1 Startzustände\nq3 = Endzustand / q3,q5 Endzustände\n',
-                'gruppe': 'Information zu Gruppen.',
-                'emre': 'Information zum Erfinder.',
-                'cyk': """CYK-Algorithmus von CNF-Grammatiken\nz.B.: /cyk baaba
-                        'S -> AB | BC\nA -> BA | a | b\nB -> CC | b\nC -> AB | a'""",
-                'crs': 'Chinesischer Restsatz\nz.B.: /crs 2,3 3,5 2,7',
-                'prf': 'Primfaktorzerlegung einer Zahl (n) und seine Eulersche Phi-Funtion\nz.B.: /prf 10',
-                'rsa_pkey': 'Berechnet den privaten Schlüssel bei gegebenen p und q und e.\nz.B.: /rsa_pkey 11 37 37',
-                'rsa_dec': 'Berechnet die originale Nachricht anhand eines öffentlichen Schlüssels (e, n) und der verschlüsselten Nachricht (c).\nz.B.: /rsa_dec 299 5 60'
-
-            }
-            command = i[1]
-
-            if command not in cmd_helps:
-                emredev.send_message(cid, 'Keine Erklärung.. 🙁')
-                return
-
-            if command == 'crs':
-                emredev.send_photo(
-                    cid, photo='https://cdn.discordapp.com/attachments/319066984748941312/871069126402142298/besteErklarung.jpg')
-            emredev.send_message(
-                cid, cmd_helps[command])
-        else:
-            emredev.send_message(
-                cid, 'Hier sind alle meine Befehle: /help, /daten, /gruppe, /emre, /minimize, /cyk, /crs, /rsa_dec, /rsa_pkey und /prf.\nUm mehr über einen Befehl zu erfahren schreibe: z.B: /help prf\n\nZudem reagiere ich auf die Keywords "Java" und "Ente" sobald diese in einem Satz vorkommen. Probier es doch aus!')
-    # minimize 5
+        helping(cid, msg)
     elif '/minimize' in msg:
         # parse string "minimize 5" to int 5
         i = msg.split(' ')
@@ -174,6 +205,19 @@ def welcome(chat_member, cid, title):
                 cid, f'Moin {member.first_name},\n\nWillkommen in der Gruppe "{title}"! 😉')
 
 
+def logging(u, cid, is_group, now_german, msg):
+    with open('cache/log.txt', 'a+') as f:
+        if is_group:
+            first_name = u.message.from_user.first_name
+            username = u.message.from_user.username
+            title = u.message.chat.title
+            f.write(
+                f'{now_german} - Group: {cid}: "{msg}"\n')
+        else:
+            f.write(
+                f'{now_german} - Person: {cid}: "{msg}"\n')
+
+
 def echo_thread(u, c):
     msg = u.message.text
     msg_orig = u.message.text
@@ -197,25 +241,10 @@ def echo_thread(u, c):
     # convert time to german time
     now_german = now.strftime('%d.%m.%Y %H:%M:%S')
 
-    with open('cache/unique_ids.txt', 'r+') as f:
-        # read file to list
-        unique_ids = list(f.read().splitlines())
-        # check if user is already in list
-        if str(cid) not in unique_ids and not is_group:
-            f.write(f'{cid}\n')
+    if msg.startswith('/'):
+        logging(u, cid, is_group, now_german, msg)
 
-    with open('cache/log.txt', 'a+') as f:
-        if is_group:
-            first_name = u.message.from_user.first_name
-            username = u.message.from_user.username
-            title = u.message.chat.title
-            f.write(
-                f'{now_german} - Group: {cid}: "{msg}"\n')
-        else:
-            f.write(
-                f'{now_german} - Person: {cid}: "{msg}"\n')
-
-    cmd_handling(msg, cid, msg_orig)
+    cmd_handling(msg, cid, msg_orig, is_group)
 
 
 def echo(u, c):
